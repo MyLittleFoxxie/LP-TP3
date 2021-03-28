88c2bdade3cd690dfa94b868c55baeee193b30a4
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-28 19:40:10 -0300

    Update intercalar.hs

--- TP3/intercalar.hs
+++ TP3/intercalar.hs
@@ -1,3 +1,16 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+
+qsort :: [Int] -> [Int]
+qsort [] = []
+qsort (a:x) = qsort [ b | b <- x, b <= a ]
+              ++ [a] ++
+              qsort [ b | b <- x, b > a ]
+
+juntar :: [Int] -> [Int] -> [Int]
+juntar [] _ = []
+juntar x y = x ++ y
+
+intercalar :: [Int] -> [Int] -> [Int]
+intercalar x y = qsort(juntar x y)
