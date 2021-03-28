0afeb9d853b7c7e7254b2633a19ffb473d0618f0
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-28 11:50:15 -0300

    Update disjuntas.hs

--- TP3/disjuntas.hs
+++ TP3/disjuntas.hs
@@ -6,46 +6,6 @@ disjuntas :: [Int] -> [Int] -> Bool
 disjuntas [] _ = True
 disjuntas _ [] = True
 disjuntas (x : xs) (y : ys)
-  | x > y = disjuntas xs (y:ys)
+  | x < y = disjuntas xs (y:ys)
   | x == y = False
-  | x < y = disjuntas (x:xs) ys
-
-
-
-
-disjuntas :: [Int] -> [Int] -> Bool
-disjuntas [] _ = True
-disjuntas _ [] = True
-disjuntas(x : xs) (y : ys) = case compare x y of
-  LT -> disjuntas xs (y : ys)
-  EQ -> False
-  GT -> disjuntas (x : xs) ys
-
-disjuntas :: [Int] -> [Int] -> Bool
-disjuntas [] _ = True
-disjuntas _ [] = True
-disjuntas (x : xs) (y : ys)
-  | x > y = disjuntas xs (y:ys)
-  | x == y = False
-  | x < y = disjuntas (x:xs) ys
-
-disjuntas [4,2] [1,4]
-
-
-nmap :: (t -> u -> z) -> [t] -> [u] -> z
-nmap f [] _ = False
-nmap f (x:xs) (y:ys) = if f x ys == False then False else if f xs ys == False then False else True
-
-
-
-disjuntas :: Int -> [Int] -> Bool
-disjuntas _ [] = False
-disjuntas x (y:ys) = if x == y then False else disjuntas x ys
-
-    | elem x ys = False
-
-qsort :: [Int] -> [Int]
-qsort [] = []
-qsort (a:x) = qsort [ b | b <- x, b <= a ]
-			  ++ [a] ++
-			  qsort [b | b <- x, b > a ]
+  | x > y = disjuntas (x:xs) ys
