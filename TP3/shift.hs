17a342fb1cc5a3bd811dfc1f04e48c57462c3327
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-28 20:00:34 -0300

    Update shift.hs

--- TP3/shift.hs
+++ TP3/shift.hs
@@ -1,3 +1,19 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+
+arredarPrimeiraParte :: [Int] -> Int -> Int -> [Int]
+arredarPrimeiraParte [] casas aux = []
+arredarPrimeiraParte (x:xs) casas aux
+  | casas > aux = arredarPrimeiraParte xs casas (aux + 1)
+  | otherwise = [x] ++ arredarPrimeiraParte xs casas (aux + 1)
+  
+arredarSegundaParte :: [Int] -> Int -> Int -> [Int]
+arredarSegundaParte [] casas aux = []
+arredarSegundaParte (x:xs) casas aux
+  | casas > aux = [x] ++ arredarSegundaParte xs casas (aux + 1)
+  | otherwise = arredarSegundaParte xs casas (aux + 1)
+  
+shift :: Int -> [Int] -> [Int]
+shift casas [] = []
+shift casas x = (arredarPrimeiraParte x casas 0) ++ (arredarSegundaParte x casas 0)
