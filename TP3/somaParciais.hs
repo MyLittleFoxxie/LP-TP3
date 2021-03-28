d5333687c29dd6efc89587ec3f208a1f7fdb6198
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-28 18:21:13 -0300

    Update somaParciais.hs

--- TP3/somaParciais.hs
+++ TP3/somaParciais.hs
@@ -2,3 +2,10 @@
 --2020/02
 --Vitor Raposo & Amanda Oliveira
 
+soma :: [Int] -> Int -> [Int]
+soma [] aux = []
+soma (x:xs) aux = (aux + x) : soma xs (aux + x)
+
+somaParciais :: [Int] -> [Int]
+somaParciais xs = soma xs 0
+
