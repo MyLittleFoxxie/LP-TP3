c8d0bc1a6e3cc780520aba221d166c050497345f
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 11:36:08 -0300

    Delete linearizar.hs

--- linearizar.hs
+++
@@ -1,7 +0,0 @@
---Código de Amanda Oliveira e Vitor Raposo sobre linearização de uma lista de inteiros 
---recebe uma lista de listas de inteiros e retorna uma lista de inteiros
-linearizar :: [[Int]] -> [Int]
--- uma lista vazia retornará outra lista vazia
-linearizar [] = []
---concatena a primeira lista com a próxima e assim sucessivamente com a reciursividade da linearizar
-linearizar (a:b) = a++linearizar b
