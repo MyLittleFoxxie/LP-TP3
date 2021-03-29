bf33425050ff669ca339873ef990087f69cf50e9
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 12:03:35 -0300

    Update disjuntas.hs

--- TP3/disjuntas.hs
+++ TP3/disjuntas.hs
@@ -1,10 +1,13 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+--Código sobre checar se duas listas sao disjuntas
 
 disjuntas :: [Int] -> [Int] -> Bool
 disjuntas [] _ = True
 disjuntas _ [] = True
+--Caso o elemento x de uma lista seja diferente que o y de outra, percorrer o resto da lista
+--Caso contrario, retornar False
 disjuntas (x : xs) (y : ys)
   | x < y = disjuntas xs (y:ys)
   | x == y = False
