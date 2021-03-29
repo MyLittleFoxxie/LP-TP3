a891565b3ac7ed39043e5905c86798b3c3a1bf78
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 12:03:20 -0300

    Update binario.hs

--- TP3/binario.hs
+++ TP3/binario.hs
@@ -1,7 +1,10 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+--Código sobre transformar um numero decimal binario
+
 
 binario :: Int -> [Int]
 binario 0 = [0]
+--`quot` divide n descartando o resto da divisao (divisoes sucessivas por 2), enquanto `rem` somente retorna o resto (impar ou par)
 binario n = binario ( n `quot` 2 ) ++ [ n `rem` 2 ]
