c26c73e911eecd6d83cc7a7239a53f51850bfd18
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 12:11:36 -0300

    Update somaImpares.hs

--- TP3/somaImpares.hs
+++ TP3/somaImpares.hs
@@ -1,8 +1,10 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+--Código sobre somar os elemntos impares de uma lista
 
 somaImpares :: [Int] -> Int
 
+--Caso o numero seja impar (divisao por modulo igual a 0), ele paticipa da soma acumulada
 somaImpares [] = 0
 somaImpares (x:xs) = if (mod x 2 == 0) then somaImpares xs else x + somaImpares xs
