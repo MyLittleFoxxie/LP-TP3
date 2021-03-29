3315ac08732190280fc6123cda1de2a0a72f93e8
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 12:04:01 -0300

    Update intercalar.hs

--- TP3/intercalar.hs
+++ TP3/intercalar.hs
@@ -1,16 +1,20 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+--Código sobre intercalar, em ordem crescente, os elementos de duas listas
 
+--Funcao para ordenar uma lista
 qsort :: [Int] -> [Int]
 qsort [] = []
 qsort (a:x) = qsort [ b | b <- x, b <= a ]
               ++ [a] ++
               qsort [ b | b <- x, b > a ]
 
+--Funcao para concatenar duas listas
 juntar :: [Int] -> [Int] -> [Int]
 juntar [] _ = []
 juntar x y = x ++ y
 
+--Concatena e ordena duas listas em uma
 intercalar :: [Int] -> [Int] -> [Int]
 intercalar x y = qsort(juntar x y)
