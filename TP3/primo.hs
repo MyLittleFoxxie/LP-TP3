d05d57804d0f498a30cf729a14cbb2563009d10c
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 12:04:14 -0300

    Update primo.hs

--- TP3/primo.hs
+++ TP3/primo.hs
@@ -1,11 +1,13 @@
 --Laboratório de Linguagens de Programação
 --2020/02
 --Vitor Raposo & Amanda Oliveira
+--Código sobre checar se um numero seria primo
 
 primo :: Int -> Bool
 primo 1 = False
 primo 2 = True
 primo n = isPrimo n (n-1)
 
+--divide um numero n por todos os numeros iguais e menores do que ele, olhando sempre o resto
 isPrimo _ 1 = True
 isPrimo n t = if (n `rem` t) == 0 then False else isPrimo n (t-1)
