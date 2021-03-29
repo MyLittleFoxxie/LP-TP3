1b497889e358b5746c6b4023a92f4f3ff26031cc
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-03-29 11:36:14 -0300

    Delete potencia.hs

--- potencia.hs
+++
@@ -1,7 +0,0 @@
---Código de Amanda Oliveira e Vitor Raposo sobre potência
---recebe 2 inteiros e retorna 1 inteiros
-potencia :: Int -> Int -> Int
--- potencia de qualquer número elevado a zero é 1
-potencia _ 0 = 1
---potencia com recursividade
-potencia a b = a * potencia a (b-1)
